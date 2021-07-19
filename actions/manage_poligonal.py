d7bf5b27427a0ecbf57f7b2ed576bd28a1f69a0d
Fran Martin <fmartinrivas2@gmail.com>
2021-07-19 13:41:55 +0200

    Add Poligonal's manager field management

--- actions/manage_poligonal.py
+++ actions/manage_poligonal.py
@@ -9,7 +9,6 @@ with the poligonal's point layer reprojected ETRS89 coordinates.
 ***************************************************************************/
 """
 
-import datetime
 import os
 
 from qgis.core import (QgsVectorLayer,
@@ -20,6 +19,7 @@ from qgis.core import (QgsVectorLayer,
                        QgsGeometry,
                        QgsProject)
 from qgis.core.additions.edit import edit
+from PyQt5.QtCore import QVariant
 from PyQt5.QtWidgets import QMessageBox
 
 from ..config import *
@@ -38,7 +38,64 @@ class ManagePoligonal:
 
     def update_poligonal_table(self):
         """   """
-        pass
+        self.set_layers()
+        self.add_fields()
+        self.populate_fields()
+
+    def set_layers(self):
+        """  """
+        self.polig_points_layer = QgsVectorLayer(os.path.join(self.doc_delim, 'Cartografia/Pto_Polig.shp'))
+        self.polig_table = QgsVectorLayer(os.path.join(self.doc_delim, 'Taules/POLIGONA.dbf'))
+
+    def add_fields(self):
+        """  """
+        self.add_fields_layer()
+        self.add_fields_table()
+
+    def add_fields_layer(self):
+        """  """
+        concat_field = QgsField(name='Concat_1', type=QVariant.String, typeName='text', len=12)
+        x_coord_field = QgsField(name='X_Coord', type=QVariant.Double)
+        y_coord_field = QgsField(name='Y_Coord', type=QVariant.Double)
+
+        new_fields_list = (x_coord_field, y_coord_field, concat_field)
+        self.polig_points_layer.dataProvider().addAttributes(new_fields_list)
+        self.polig_points_layer.updateFields()
+
+    def add_fields_table(self):
+        """  """
+        concat_field = QgsField(name='Concat_2', type=QVariant.String, typeName='text', len=12)
+
+        self.polig_table.dataProvider().addAttributes([concat_field])
+        self.polig_table.updateFields()
+
+    def populate_fields(self):
+        """  """
+        self.populate_fields_layer()
+        self.populate_fields_table()
+
+    def populate_fields_layer(self):
+        """  """
+        with edit(self.polig_points_layer):
+            fields = self.polig_points_layer.fields()
+            for feature in self.polig_points_layer.getFeatures():
+                concat = f"{feature['ID_POLIG']}_{feature['ID_VIS']}"
+                attrs = {
+                    fields.indexFromName("X_Coord"): feature.geometry().asPoint()[0],
+                    fields.indexFromName("Y_Coord"): feature.geometry().asPoint()[1],
+                    fields.indexFromName("Concat_1"): concat
+                }
+                self.polig_points_layer.dataProvider().changeAttributeValues({feature.id(): attrs})
+
+    def populate_fields_table(self):
+        """  """
+        with edit(self.polig_table):
+            fields = self.polig_table.fields()
+            id_field = fields.indexFromName("Concat_2")
+            for feature in self.polig_table.getFeatures():
+                concat = f"{feature['ID_POLIG']}_{feature['ID_VIS']}"
+                attr = {id_field: concat}
+                self.polig_table.dataProvider().changeAttributeValues({feature.id(): attr})
 
     def check_input_data(self):
         """ Check that exists all the necessary input data into the input directory """
