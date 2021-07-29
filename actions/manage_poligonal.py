59f53e3394a505a9964848933c6d54bff1f2530e
Fran Martin <fmartinrivas2@gmail.com>
2021-07-29 11:10:36 +0200

    feat: finish the manage poligonal module

--- actions/manage_poligonal.py
+++ actions/manage_poligonal.py
@@ -13,20 +13,14 @@ import os
 
 from qgis.core import (QgsVectorLayer,
                        QgsField,
-                       QgsMessageLog,
                        QgsVectorLayerJoinInfo)
-import processing
-from PyQt5.QtWidgets import QMessageBox
 from qgis.core.additions.edit import edit
 from PyQt5.QtCore import QVariant
 from PyQt5.QtWidgets import QMessageBox
 
-from ..config import *
-
-
-# TODO TEST
 
 class ManagePoligonal:
+    """ Poligonal managing class """
 
     def __init__(self, doc_delim_directory):
         # Layers and paths
@@ -37,29 +31,33 @@ class ManagePoligonal:
         self.box_error.setIcon(QMessageBox.Critical)
 
     def update_poligonal_table(self):
-        """   """
+        """
+        Main entry point where the poligonal gets updated, which means that the poligonal's points coordinates
+        of the POLIGONA.dbf table get reprojected from ED50 to ETRS88.
+        The method only updates the attributes X/Y COMP and X/Y CONV.
+        """
         self.set_layers()
         self.add_fields()
         self.populate_fields()
         self.join()
         self.update()
-        self.polig_table.removeJoin(self.join_object.joinLayerId)
+        self.polig_table.removeJoin(self.join_object.joinLayerId())
         self.remove_fields()
 
     def set_layers(self):
-        """  """
+        """ Set the necessary Vector layers """
         self.polig_points_layer = QgsVectorLayer(os.path.join(self.doc_delim, 'Cartografia/Pto_Polig.shp'))
         self.polig_table = QgsVectorLayer(os.path.join(self.doc_delim, 'Taules/POLIGONA.dbf'))
 
     # #######################
     # Add fields
     def add_fields(self):
-        """  """
+        """ Add the necessary fields to both poligonal's layer and table """
         self.add_fields_layer()
         self.add_fields_table()
 
     def add_fields_layer(self):
-        """  """
+        """ Add the necessary fields to the poliognal's point layer """
         concat_field = QgsField(name='Concat_1', type=QVariant.String, typeName='text', len=12)
         x_coord_field = QgsField(name='X_Coord', type=QVariant.Double, len=19, prec=5)
         y_coord_field = QgsField(name='Y_Coord', type=QVariant.Double, len=19, prec=5)
@@ -69,7 +67,7 @@ class ManagePoligonal:
         self.polig_points_layer.updateFields()
 
     def add_fields_table(self):
-        """  """
+        """ Add the necessary fields to the poligonal's table """
         concat_field = QgsField(name='Concat_2', type=QVariant.String, typeName='text', len=12)
 
         self.polig_table.dataProvider().addAttributes([concat_field])
@@ -78,12 +76,12 @@ class ManagePoligonal:
     # #######################
     # Populate fields
     def populate_fields(self):
-        """  """
+        """ Populate the new fields of both poligonal's layer and table """
         self.populate_fields_layer()
         self.populate_fields_table()
 
     def populate_fields_layer(self):
-        """  """
+        """ Populate the new fields of the poligonal's layer """
         with edit(self.polig_points_layer):
             fields = self.polig_points_layer.fields()
             for feature in self.polig_points_layer.getFeatures():
@@ -98,7 +96,7 @@ class ManagePoligonal:
                 self.polig_points_layer.dataProvider().changeAttributeValues({feature.id(): attrs})
 
     def populate_fields_table(self):
-        """  """
+        """ Populate the new fields of the poligonal's table+ """
         with edit(self.polig_table):
             fields = self.polig_table.fields()
             id_field = fields.indexFromName("Concat_2")
@@ -110,12 +108,12 @@ class ManagePoligonal:
     # #######################
     # Join
     def join(self):
-        """  """
+        """ Perform and add a left join to the poligonal's table, adding the poligonal's layer """
         # Join parameters
         self.join_object = QgsVectorLayerJoinInfo()
-        self.join_object.joinLayerId = self.polig_points_layer.id()
-        self.join_object.joinFieldName = 'Concat_1'
-        self.join_object.targetFieldName = 'Concat_2'
+        self.join_object.setJoinLayerId(self.polig_points_layer.id())
+        self.join_object.setJoinFieldName('Concat_1')
+        self.join_object.setTargetFieldName('Concat_2')
         self.join_object.setJoinLayer(self.polig_points_layer)
         self.join_object.memoryCache = True
 
@@ -125,12 +123,8 @@ class ManagePoligonal:
     # #######################
     # Update
     def update(self):
-        """  """
+        """ Update the poligonal's table with the poligonal's layer values of the reprojected coordinates """
         # Fields parameters
-        field_names = [field.name() for field in self.polig_table.fields()]
-        for field in field_names:
-            QgsMessageLog.logMessage(field)
-
         fields = self.polig_table.fields()
         id_x_comp = fields.indexFromName("X_COMP")
         id_y_comp = fields.indexFromName("Y_COMP")
@@ -163,12 +157,12 @@ class ManagePoligonal:
     # #######################
     # Remove fields
     def remove_fields(self):
-        """  """
+        """ Remove the fields added before to perform the process """
         self.remove_fields_layer()
         self.remove_fields_table()
 
     def remove_fields_layer(self):
-        """  """
+        """ Remove the fields added to the poligonal's layer """
         fields = self.polig_points_layer.fields()
         delete_fields_list = list((fields.indexFromName("X_Coord"), fields.indexFromName("Y_Coord"), fields.indexFromName("Concat_1")))
 
@@ -176,7 +170,7 @@ class ManagePoligonal:
             self.polig_points_layer.dataProvider().deleteAttributes(delete_fields_list)
 
     def remove_fields_table(self):
-        """  """
+        """ Remove the fields added to the poligonal's table """
         fields = self.polig_table.fields()
         delete_field = fields.indexFromName("Concat_2")
 
@@ -186,7 +180,7 @@ class ManagePoligonal:
     # #######################
     # Check
     def check_input_data(self):
-        """ Check that exists all the necessary input data into the input directory """
+        """ Check that exist all the necessary input data into the input directory """
         cartography_directory = os.path.join(self.doc_delim, 'Cartografia')
         tables_directory = os.path.join(self.doc_delim, 'Taules')
 
